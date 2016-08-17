c0d12a17261653fc204f37fd650f87fc97f8b514
Chris Jarvis <clj@ollure.com>
2016-08-16 23:33:15 -0500

    functional test attempt, not compling

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVistorTest(unittest.TestCase):
@@ -13,11 +14,25 @@ class NewVistorTest(unittest.TestCase):
     self.browser.get('http://localhost:8000')
 
     self.assertIn('To-Do', self.browser.title)
-    self.fail('Finish the test!')
+    header_text = self.browser.find_element_by_tag_name('h1').text
+    self.assertIn('To-Do', header_text)
+
+    #user is invited to enter a to-do item straight away
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+    #User types an action in to test box
+    inputbox.send_keys(Keys.ENTER)
+
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_element_by_tag_name('tr')
+    self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+    self.fail('Finsh the test')
 
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
+
 #assert 'To Do LIST' in browser.title
 
 #browser.quit()
